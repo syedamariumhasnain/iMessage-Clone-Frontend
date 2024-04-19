599b56fb2f30eb62f9ce0ba07b08cedab0561327
syedamariumhasnain <syedamarium27@gmail.com>
2024-04-19 14:00:50 +0500

    chakra ui -- theme setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
+import { theme } from "../chakra/theme";
 
 export default function App({
   Component,
@@ -7,7 +9,9 @@ export default function App({
 }: AppProps) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </SessionProvider>
   );
 }
