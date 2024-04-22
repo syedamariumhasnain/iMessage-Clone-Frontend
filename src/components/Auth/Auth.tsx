eb25dc92f0770d15f4edb694c89644a7199fc450
syedamariumhasnain <syedamarium27@gmail.com>
2024-04-22 13:17:24 +0500

    basic chat, auth components

---
+++ src/components/Auth/Auth.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@chakra-ui/react";
+import { signIn } from "next-auth/react";
+
+interface IAuthProps {}
+
+const Auth: React.FC<IAuthProps> = (props) => {
+  return (
+    <div>
+      AUTH
+      <Button onClick={() => signIn("google")}>Sign In</Button>
+    </div>
+  );
+};
+
+export default Auth;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,15 @@
+import { Box } from "@chakra-ui/react";
 import type { NextPage, NextPageContext } from "next";
-import { getSession, signIn, signOut, useSession } from "next-auth/react";
+import { getSession, useSession } from "next-auth/react";
+import Chat from "../components/Chat/Chat";
+import Auth from "../components/Auth/Auth";
 
 const Home: NextPage = () => {
   const { data } = useSession();
 
   console.log("HERE IS DATA", data);
 
-  return (
-    <div>
-      {data?.user ? (
-        <button onClick={() => signOut()}>Sign Out</button>
-      ) : (
-        <button onClick={() => signIn("google")}>Sign In</button>
-      )}
-      {data?.user?.name}
-    </div>
-  );
+  return <Box>{data?.user ? <Chat /> : <Auth />}</Box>;
 };
 
 export async function getServerSideProps(context: NextPageContext) {
