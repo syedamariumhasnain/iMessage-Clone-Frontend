57edd95d81c6ed1c99e068c1cb5c49e6ec70e622
syedamariumhasnain <syedamarium27@gmail.com>
2024-04-22 13:20:34 +0500

    adding custom properties to user object in next-auth session

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ const Home: NextPage = () => {
 
   console.log("HERE IS DATA", data);
 
-  return <Box>{data?.user ? <Chat /> : <Auth />}</Box>;
+  return <Box>{data?.user?.username ? <Chat /> : <Auth />}</Box>;
 };
 
 export async function getServerSideProps(context: NextPageContext) {
